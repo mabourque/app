f0349d65937fb331e6d48605a9433e6bfadee24a
mabourque <mabourque@mbcomm.ca>
2024-12-23 18:34:30 -0500

    Create page.tsx

---
+++ page.tsx
@@ -0,0 +1,9 @@
+import ServiceCallForm from './components/ServiceCallForm'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-4">
+      <ServiceCallForm />
+    </main>
+  )
+}
