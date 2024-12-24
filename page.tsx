889e4e83025207bd3d72a8b1f479433b05bd271b
mabourque <mabourque@mbcomm.ca>
2024-12-24 10:48:34 -0500

    Update page.tsx

--- page.tsx
+++ page.tsx
@@ -1,9 +1,11 @@
 import ServiceCallForm from './components/ServiceCallForm'
 
-export default function Home() {
+function App() {
   return (
     <main className="min-h-screen p-4">
       <ServiceCallForm />
     </main>
   )
 }
+
+export default App
